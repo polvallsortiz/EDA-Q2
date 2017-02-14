ea1aa036afb627c4ad9190bba0e9b2ec94661001
polvallsortiz <pol.valls.ortiz@est.fib.upc.edu>
2017-02-14 11:12:29 +0100

    P50709

--- USEOFSTLDATASTRUCTURES/P50709.cc
+++ USEOFSTLDATASTRUCTURES/P50709.cc
@@ -8,21 +8,43 @@ int main() {
     while(cin >> op) {
         switch (op)
         {
-            case 'S:
+            case 'S':
                 int temp;
                 cin >> temp;
-                prior.push(cin)
+                prior.push(temp);
+                break;
+
             case 'A':
                 if(not prior.empty()) cout << prior.top() << endl;
                 else cout << "error!" << endl;
                 break;
 
             case 'R':
-                if(not.prior.empty())  prior.pop();
+                if(not prior.empty())  prior.pop();
                 else cout << "error!" << endl;
                 break;
 
-            case ''
+            case 'I':
+                int temp2;
+                cin >> temp2;
+                if(not prior.empty()) {
+                    int aux = prior.top();
+                    prior.pop();
+                    prior.push(aux + temp2);
+                }
+                else cout << "error!" << endl;
+                break;
+
+            case 'D':
+                int temp3;
+                cin >> temp3;
+                if(not prior.empty()) {
+                    int aux2 = prior.top();
+                    prior.pop();
+                    prior.push(aux2-temp3);
+                }
+                else cout << "error!" << endl;
+                break;
         }
     }
 }
