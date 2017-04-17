d520ab8f98cd1651af215507bcaf321dba50e21b
polvallsortiz <pol.valls.ortiz@est.fib.upc.edu>
2017-04-17 16:19:30 +0200

    P33412 Only working disordered elements next to middle element

--- DIVIDECONQUER/P33412.cc
+++ DIVIDECONQUER/P33412.cc
@@ -8,10 +8,12 @@ using namespace std;
 int pos_search(double x, const vector<double>& v, int left, int right) {
 	int middle = (left + right) / 2;
 	if(left > right) return -1;
-	else {
-
+	else if(v[middle] == x) return middle;
+	else if(v[middle] < x) { //MIDDLE POT SER ELEMENT NO ORDENAT ASC
+		if(v[middle+1] <= x) return pos_search(x,v,middle+1,right);
+		else if(v[middle-1] >= x) return pos_search(x,v,left,middle-1);
 	}
-
+	else if(v[middle] > x) return pos_search(x,v,left,middle);
 }
 
 bool resistant_search(double x, const vector<double>& v) {
@@ -21,7 +23,7 @@ bool resistant_search(double x, const vector<double>& v) {
 
 int main() {
 	vector<double> v(8);
-	v[0] = 2;v[1] = 3.2;v[2] = 5;v[3] = 6;v[4] = 4;v[5] = 8.2;v[6] = 9.7;v[7] = 10;
+	v[0] = 2;v[1] = 3.2;v[2] = 5;v[3] = 4;v[4] = 6;v[5] = 8.2;v[6] = 9.7;v[7] = 10;
 	double abuscar;
 	cin >> abuscar;
 	if(resistant_search(abuscar,v)) cout << "ESTA" << endl;
